6fca50fd256d09a1985792fff5ca9de4a7e2e06b
Mangobox <94897284+Mangoboxlabs@users.noreply.github.com>
2022-07-18 14:02:16 +0800

    Create TokenRepresentationProxy.sol

---
+++ contracts/TokenRepresentationProxy.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "./TicketBooth.sol";
+
+contract TokenRepresentationProxy is ERC20 {
+    ITicketBooth ticketBooth;
+    uint256 projectId;
+
+    constructor(
+        ITicketBooth _ticketBooth,
+        uint256 _projectId,
+        string memory name,
+        string memory ticker
+    ) ERC20(name, ticker) {
+        ticketBooth = _ticketBooth;
+        projectId = _projectId;
+    }
+
+    function totalSupply() public view virtual override returns (uint256) {
+        return ticketBooth.totalSupplyOf(projectId);
+    }
+
+    function balanceOf(address _account) public view virtual override returns (uint256) {
+        return ticketBooth.balanceOf(_account, projectId);
+    }
+}
